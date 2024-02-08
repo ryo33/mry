2090beebc75f2c73993398998baaf25f57ed870b
Ryo Hirayama <ryo12redstone@gmail.com>
2024-02-08 12:58:29 +0900

    Refactor package structure for internal crates for integration testing

--- mry/tests/crate_bound_consumer/src/lib.rs
+++ mry/tests/crate_bound_consumer/src/lib.rs
@@ -1,7 +1,7 @@
 #[test]
 fn can_use_trait_declared_in_outer_crate() {
-    use crate_bound::Foo as _;
-    let mut mock = crate_bound::MockFoo::default();
+    use mry_crate_bound::Foo as _;
+    let mut mock = mry_crate_bound::MockFoo::default();
     mock.mock_foo().returns(42);
     assert_eq!(mock.foo(), 42);
 }
